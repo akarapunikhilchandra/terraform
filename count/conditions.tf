d72abe8e8d55663a86b76a9815c74f5613795870
nikhil <anc@gmail.com>
2023-09-10 12:40:12 +0530

    loops

--- count/conditions.tf
+++ count/conditions.tf
@@ -1,8 +1,17 @@
-resource "aws_instance" "myowninstance" {
+resource "aws_instance" "conditions" {
     count = 10
     ami = var.ami_id
     instance_type = var.instance_names[count.index] == "mongoDB" || var.instance_names[count.index] == "mysql" ? "t3.medium" : "t2.micro"
     tags = {
         Name=var.instance_names[count.index]
     }
+}
+
+resource "aws_route53_record" "record" {
+    count = 10
+    zone_id = var.zone_id
+    name    = "${var.instance_names[count.index]}.${var.domain}"
+    type    = "A"
+    ttl     = 1
+    records = [aws_instance.conditions[count.index].private_ip]
 }
--- count/variables.tf
+++ count/variables.tf
@@ -11,3 +11,11 @@ variable "instance_names" {
     type = list
     default = ["mongoDB", "cart", "catalogue", "user", "redis", "mysql", "rabbitmq", "shipping", "payment", "web"]
 }
+
+variable "zone_id" {
+    default = "Z10334032ZMZZ509Q21DU"
+}
+
+variable "domain" {
+    default = "joindevops.cloud"
+}
