50b2046fe4117f0d64945d2490a63555a8bc43d4
nikhil <anc@gmail.com>
2023-09-11 15:56:28 +0530

    locals

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,4 +1,6 @@
 locals {
     ami_id = "ami-0f409bae3775dc8e5"
     instance_type = var.isPROD ? "t3.micro" : "t2.micro"
+    public_key = file("${path.module}/ansible.pub")
+    key_name   = "ansible.pub"
 }
--- locals/test-locals.tf
+++ locals/test-locals.tf
@@ -2,3 +2,8 @@ resource "aws_instance" "file-function" {
     ami = local.ami_id 
     instance_type = local.instance_type
 }
+
+resource "aws_key_pair" "deployer" {
+  key_name   = local.key_name
+  public_key = local.public_key
+}
