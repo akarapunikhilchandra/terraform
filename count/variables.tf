5521b20ab127a50db19bb95a0cc45443a52f68f8
nikhil <anc@gmail.com>
2023-09-11 22:27:38 +0530

    all

--- count/variables.tf
+++ count/variables.tf
@@ -1,18 +1,15 @@
-#variable "instance_name" {
-#    default = "cart"
-#}
-
 variable "ami_id" {
     type = string
-    default = "ami-03265a0778a880afb"
+    default = "ami-0f409bae3775dc8e5"
 }
 
 variable "instance_names" {
-    type = list
-    default = ["mongoDB", "cart", "catalogue", "user", "redis", "mysql", "rabbitmq", "shipping", "payment", "web"]
+    type = list 
+    default = ["mongodb", "cart", "catalogue", "user", "redis", "mysql", "rabbitmq", "shipping", "payment", "web"]
 }
 
 variable "zone_id" {
+    type = string
     default = "Z10334032ZMZZ509Q21DU"
 }
 
