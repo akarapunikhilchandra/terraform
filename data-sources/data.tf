b37a933bae58fdbc188397f0fcf6d5bb94086e26
nikhil <anc@gmail.com>
2023-09-12 11:32:35 +0530

    securitygroupusing-data-resources

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,13 +1,12 @@
-data "aws_ami" "ami_id" {
-    most_recent = true
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-    }
-}
+# data "aws_ami" "ami_id" {
+#     most_recent = true
+#     owners = ["amazon"]
+#     filter {
+#         name = "name"
+#         values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+#     }
+# }
 
-output "ami_id" {
-    value = data.aws_ami.ami_id.id  
-}
-
+# output "ami_id" {
+#     value = data.aws_ami.ami_id.id  
+# }
--- data-sources/sample-data-sources.tf
+++ data-sources/sample-data-sources.tf
@@ -5,3 +5,23 @@ data "aws_vpc" "default" {
 output "vpc_info" {
     value = data.aws_vpc.default  
 }
+
+resource "aws_security_group" "allow-http" {
+    name = var.sg_name
+    vpc_id = data.aws_vpc.default.id
+        description = "allowing port 80"
+        ingress {
+            description = "allowing all inbound traffic"
+            from_port = 80
+            to_port = 80 
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+      
+        egress {
+            from_port = 0
+            to_port = 0
+            protocol = "-1" 
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
