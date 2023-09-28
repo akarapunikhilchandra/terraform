7bf4b2af5461d1d357e2b6b70f95bed1ddad3e59
nikhil <anc@gmail.com>
2023-09-28 21:43:49 +0530

    all

--- terraform-aws-vpc-advanced/peering.tf
+++ terraform-aws-vpc-advanced/peering.tf
@@ -1,54 +1,42 @@
-### VPC Peering with Default VPC
 resource "aws_vpc_peering_connection" "peering" {
   count = var.is_peering_required ? 1 : 0
-  #peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = aws_vpc.main.id
-  #requestor, default VPC is our requestor
+  #peer_owner_id = "${var.peer_owner_id}"
+  peer_vpc_id   = aws_vpc.main.id 
+  #vpc_id is our requestor id
   vpc_id        = var.requestor_vpc_id
-  auto_accept   = true
-
+  auto_accept = true  
+  
   tags = merge(
     {
-      Name = "VPC Peering between Default and ${var.project_name}"
+      Name = "peering between default and ${var.project_name}"
     },
     var.common_tags
   )
 }
+resource "aws_route" "default_route" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = var.default_route_table_id 
+  destination_cidr_block    = var.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id 
+}
 
-# resource "aws_route" "default_route" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = var.default_route_table_id
-#   destination_cidr_block    = var.cidr_block
-#   # since we set count parameter, it is treated as list, even single element you should write list syntax
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-#   #depends_on                = [aws_route_table.testing]
-# }
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id   
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id 
+}
 
-# #add route in roboshop public route table
-# resource "aws_route" "public_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = aws_route_table.public.id
-#   destination_cidr_block    = var.default_vpc_cidr
-#   # since we set count parameter, it is treated as list, even single element you should write list syntax
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-#   #depends_on                = [aws_route_table.testing]
-# }
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id   
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id 
+}
 
-
-# resource "aws_route" "private_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = aws_route_table.private.id
-#   destination_cidr_block    = var.default_vpc_cidr
-#   # since we set count parameter, it is treated as list, even single element you should write list syntax
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-#   #depends_on                = [aws_route_table.testing]
-# }
-
-# resource "aws_route" "database_peering" {
-#   count = var.is_peering_required ? 1 : 0
-#   route_table_id            = aws_route_table.database.id
-#   destination_cidr_block    = var.default_vpc_cidr
-#   # since we set count parameter, it is treated as list, even single element you should write list syntax
-#   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
-#   #depends_on                = [aws_route_table.testing]
-# }
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id   
+  destination_cidr_block    = var.default_vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id 
+}
