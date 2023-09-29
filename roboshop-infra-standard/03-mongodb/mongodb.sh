87517b1fc5071682a87e117da08e119905b9fc91
nikhil <anc@gmail.com>
2023-09-29 17:11:47 +0530

    s3

--- roboshop-infra-standard/03-mongodb/mongodb.sh
+++ roboshop-infra-standard/03-mongodb/mongodb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /tmp
-git clone git@github.com:akarapunikhilchandra/roboshop-shell.git
+git clone https://github.com/akarapunikhilchandra/roboshop-shell.git
 cd roboshop-shell
 sh mongodb.sh
