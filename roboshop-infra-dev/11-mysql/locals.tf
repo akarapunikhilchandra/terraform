56dd7197c96d2762c87720601f118e8a302b7aa7
nikhil <anc@gmail.com>
2023-10-17 13:04:27 +0530

    all

--- roboshop-infra-dev/11-mysql/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-    db_subnet_id = element(split(",", data.aws_ssm_parameter.database_subnet_ids.value),0)
-}
--- roboshop-infra-dev/12-shipping/shipping.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/centos
-git clone https://github.com/akarapunikhilchandra/roboshop-shell-tf.git
-cd roboshop-shell-tf
-sh shipping.sh
