34a143761860d3102cb74a3dea9ce8a396fdc9ef
nikhil <anc@gmail.com>
2023-10-30 22:49:25 +0530

    all

--- roboshop-infra-prod/01.1-firewalls/variables.tf
+++ roboshop-infra-prod/01.1-firewalls/variables.tf
@@ -4,7 +4,7 @@ variable "project_name" {
 variable "common_tags" {
   default = {
     Project = "roboshop"
-    #component = "firewalls-dev"
+    component = "firewalls-dev"
     Environment = "PROD"
     Terraform = "true"
   }
